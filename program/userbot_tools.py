651cbdc5036d3c2373d4bb6b6c59d089b6e5f25a
Aliyevdii <95511686+Aliyevdii@users.noreply.github.com>
2021-12-13 21:12:51 +0400

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -23,7 +23,7 @@ async def join_chat(c: Client, m: Message):
             (await user.get_me()).id,
             can_manage_voice_chats=True
         )
-        return await user.send_message(chat_id, "✅ userbot entered chat")
+        return await user.send_message(chat_id, "✅ userbot söhbətə daxil oldu")
     except UserAlreadyParticipant:
         admin = await m.chat.get_member((await user.get_me()).id)
         if not admin.can_manage_voice_chats:
@@ -31,8 +31,8 @@ async def join_chat(c: Client, m: Message):
                 (await user.get_me()).id,
                 can_manage_voice_chats=True
             )
-            return await user.send_message(chat_id, "✅ userbot already in chat")
-        return await user.send_message(chat_id, "✅ userbot already in chat")
+            return await user.send_message(chat_id, "✅ userbot artıq söhbətdədir")
+        return await user.send_message(chat_id, "✅ userbot artıq söhbətdədir")
 
 
 @Client.on_message(command(["userbotleave",
@@ -45,7 +45,7 @@ async def leave_chat(_, m: Message):
         await user.leave_chat(chat_id)
         return await _.send_message(
             chat_id,
-            "✅ userbot leaved chat",
+            "✅ userbot söhbəti tərk etdi",
         )
     except UserNotParticipant:
         return await _.send_message(
